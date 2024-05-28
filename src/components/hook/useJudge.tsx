376c3239e1ceeabf79967ed83ab49c3aedba347d
tenxswej <144094673+tenxswej@users.noreply.github.com>
2024-05-28 18:35:54 +0630

    changed:

--- src/components/hook/useJudge.tsx
+++ src/components/hook/useJudge.tsx
@@ -1,27 +1,17 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-export const useJudge = (total : number, correct  : number) => {
+export const useJudge = (total: number, correct: number) => {
+   const [result, setResult] = useState<"Good" | "Bad">("Good");
 
-    const [result, setResult] = useState<"Good" | "Bad">("Good")
+   useEffect(() => {
+      const isAboveAverage = Math.floor(total / 2) < correct;
 
+      if (isAboveAverage) {
+         setResult("Good");
+      } else {
+         setResult("Bad");
+      }
+   }, [total, correct]);
 
-    useEffect(() => {
-
-        const isAboveAverage = Math.floor(total / 2) < correct
-
-        if(isAboveAverage) {setResult("Good")}
-        else{
-
-            setResult("Bad")
-        }
-
-        
-
-    },[total, correct])
-
-
-    return [result]
-
-
-
-}
+   return [result];
+};
